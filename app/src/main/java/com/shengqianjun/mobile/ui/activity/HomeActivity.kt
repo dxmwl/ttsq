55f0e9f41eb931b37448e7c337a3a105f37021c5
clbDream <3214341986@qq.com>
2022-12-07 17:57:03 +0800

    增加首页淘口令识别

--- app/src/main/java/com/shengqianjun/mobile/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/shengqianjun/mobile/ui/activity/HomeActivity.kt
@@ -1,22 +1,39 @@
 package com.shengqianjun.mobile.ui.activity
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.blankj.utilcode.util.ActivityUtils
+import com.blankj.utilcode.util.ClipboardUtils
+import com.blankj.utilcode.util.ConvertUtils
+import com.blankj.utilcode.util.ScreenUtils
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.gyf.immersionbar.ImmersionBar
 import com.hjq.base.FragmentPagerAdapter
 import com.hjq.http.EasyHttp
 import com.hjq.http.listener.OnHttpListener
+import com.orhanobut.logger.Logger
 import com.shengqianjun.mobile.R
 import com.shengqianjun.mobile.app.AppActivity
 import com.shengqianjun.mobile.app.AppFragment
 import com.shengqianjun.mobile.app.AppHelper
 import com.shengqianjun.mobile.eventbus.RefreshClass
 import com.shengqianjun.mobile.http.api.ClassApi
+import com.shengqianjun.mobile.http.api.GetYouhuiApi
 import com.shengqianjun.mobile.http.model.HttpData
 import com.shengqianjun.mobile.manager.ActivityManager
 import com.shengqianjun.mobile.other.DoubleClickHelper
@@ -200,5 +217,94 @@ class HomeActivity : AppActivity(), NavigationAdapter.OnNavigationListener {
                 }
             }
             )
+    }
+
+    //显示商品信息
+    @SuppressLint("SetTextI18n")
+    private fun showGoodsInfoDialog(infoData: GetYouhuiApi.GoodsYouhuiDto?) {
+        if (infoData == null) {
+            return
+        }
+        val topActivity = ActivityUtils.getTopActivity()
+        val inflate =
+            LayoutInflater.from(topActivity).inflate(R.layout.dialog_show_goods_info, null)
+        inflate.findViewById<TextView>(R.id.goods_name)?.text = infoData.tao_title
+        inflate.findViewById<TextView>(R.id.shop_name)?.text = infoData.shop_title
+        inflate.findViewById<TextView>(R.id.qhj)?.text = infoData.quanhou_jiage
+//        inflate.findViewById<TextView>(R.id.ygsy)?.text = "${infoData.fan}"
+        inflate.findViewById<TextView>(R.id.xl)?.text = infoData.volume
+        val goodsImg = inflate.findViewById<ImageView>(R.id.goods_img)
+        val shopIcon = inflate.findViewById<ImageView>(R.id.shop_icon)
+        val tvYhq = inflate.findViewById<TextView>(R.id.yhq_price)
+        if (infoData.coupon_info_money == "0" || infoData.coupon_info_money == "0.0" || infoData.coupon_info_money == "0.00") {
+            tvYhq.visibility = View.GONE
+        } else {
+            tvYhq?.text = "${infoData.coupon_info_money}元券"
+        }
+        Glide.with(topActivity).load(infoData.pict_url).apply(
+            RequestOptions().transforms(
+                CenterCrop(),
+                RoundedCorners(ConvertUtils.dp2px(5F))
+            )
+        ).into(goodsImg)
+        val options = RequestOptions()
+            .error(R.mipmap.ic_launcher)
+        Glide.with(topActivity).load(infoData.shopIcon).apply(options).into(shopIcon)
+
+        val goodsDialog = AlertDialog.Builder(topActivity)
+            .setView(inflate)
+            .setCancelable(false)
+            .show()
+        val window = goodsDialog?.window
+        if (window != null) {
+            window.setBackgroundDrawableResource(android.R.color.transparent)
+            val p: WindowManager.LayoutParams = window.attributes //获取对话框当前的参数值
+            val screenWidth = ScreenUtils.getScreenWidth()
+            p.width = (screenWidth * 0.8).toInt()
+            window.attributes = p //设置生效
+        }
+        inflate.findViewById<TextView>(R.id.copy_kl)?.setOnClickListener {
+            /*ClipboardUtils.copyText(infoData.tkl)
+            ToastUtils.showShort("淘口令复制成功")*/
+            goodsDialog?.dismiss()
+            ClipboardUtils.clear()
+        }
+        inflate.findViewById<TextView>(R.id.lqyh)?.setOnClickListener {
+            BrowserActivity.start(this@HomeActivity, infoData.coupon_click_url)
+            goodsDialog?.dismiss()
+            ClipboardUtils.clear()
+        }
+        goodsDialog?.setOnDismissListener {
+            Logger.d("清空剪切板")
+            ClipboardUtils.clear()
+        }
+    }
+
+    private fun getYouhuiInfo(contentStr:String) {
+        EasyHttp.get(this)
+            .api(GetYouhuiApi().apply {
+                tkl = contentStr
+            })
+            .request(object : OnHttpListener<HttpData<ArrayList<GetYouhuiApi.GoodsYouhuiDto>>> {
+                override fun onSucceed(result: HttpData<ArrayList<GetYouhuiApi.GoodsYouhuiDto>>?) {
+                    result?.getData()?.get(0)?.let {
+                        showGoodsInfoDialog(it)
+                    }
+                }
+
+                override fun onFail(e: Exception?) {
+                    toast(e?.message)
+                }
+            })
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val clipboardContent = ClipboardUtils.getText()
+        if (clipboardContent.isNullOrBlank().not()) {
+            val toString = clipboardContent.toString()
+            getYouhuiInfo(toString)
+        }
     }
 }
